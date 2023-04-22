048c62899151f89598ad6ca0b82469f1920fbcb4
Amal Manoj <amalmanoj02@gmail.com>
2023-04-22 22:49:49 +0530

    moved widgets to components

---
+++ lumos_app/lib/components/navbar.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class NavBar extends StatelessWidget {
+  const NavBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      items: const <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: Icon(Icons.call),
+          label: 'Calls',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.camera),
+          label: 'Camera',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.chat),
+          label: 'Chats',
+        ),
+      ],
+    );
+  }
+}
