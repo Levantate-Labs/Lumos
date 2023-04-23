059640689cc6ebd56e27d6b8630e1fe10f0f21ca
Anoop072001 <anoopsidhan777@gmail.com>
2023-04-24 01:37:18 +0530

    Added Controllers

---
+++ lumos_app/lib/screens/newPost/new_event.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class NewEvent extends StatelessWidget {
+  const NewEvent({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: const Text("New Event"));
+  }
+}
---
+++ lumos_app/lib/screens/register_event_page.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:lumos_app/components/AuthInputs.dart';
+import 'package:lumos_app/controller/test_controller.dart';
+
+class RegisterEventPage extends StatelessWidget {
+  RegisterEventPage({
+    super.key,
+  });
+
+  final RegController reg = RegController();
+
+  final String eventImg =
+      'http://www.onlylondon.properties/application/modules/themes/views/default/assets/images/image-placeholder.png';
+
+  final String eventName = "HackAthena";
+  final String desc = "HackAthena is te first djfhsjdh";
+  final String venue = "Jyothi Engineering College";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(4.0),
+            child: Card(
+              elevation: 4,
+              child: Container(
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(8),
+                  child: Image(
+                    image: NetworkImage(eventImg),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(4.0),
+            child: Container(
+              width: 390,
+              alignment: Alignment.centerLeft,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    eventName,
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.w600),
+                  ),
+                  Text(
+                    venue,
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.w300),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Container(
+                    width: MediaQuery.of(context).size.width * 0.9,
+                    child: Text(
+                      desc,
+                      style:
+                          TextStyle(fontSize: 16, fontWeight: FontWeight.w300),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Obx(
+                    () => (FloatingActionButton.extended(
+                      onPressed: () => reg.toggleReg(),
+                      label: reg.attend.value == 0
+                          ? Text('Register')
+                          : Text('Registered'),
+                    )),
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
